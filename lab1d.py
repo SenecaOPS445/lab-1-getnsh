a19de4250458989bddbb8982b65478cb6f7ddae0
Geetansh Chawla <gchawla4@myseneca.ca>
2024-05-17 17:47:12 -0400

    added lab1d

---
+++ lab1d.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# Define the required objects
+x = 11
+y = 3
+z = 6
+
+# Perform the calculation
+result = x + y * z
+
+# Print the result 
+print(f'{x} + {y} * {z} = {result}')
